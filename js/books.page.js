23ad9a6fb17212ffd00f9020c71a82456f64b0cd
AndreasBach <andreasbach94@gmail.com>
2016-12-12 17:51:24 +0100

    Version 2 - igang med opret bruger funktion

--- js/books.page.js
+++ js/books.page.js
@@ -3,18 +3,17 @@ $(document).ready(function () {
   //Fires on page-load
   SDK.Book.getAll(function(err, data){
     if(err) throw err;
-      
+
     var $booksTableBody = $("#booksTableBody");
     data.forEach(function (book, i) {
 
       $booksTableBody.append(
         "<tr>" +
           "<td>" + book.title + "</td>" +
-          "<td>" + book.subtitle  + "</td>" +
-          "<td>" + printAuthors(book.authors) + "</td>" +
-          "<td>" + book.publisher.name + "</td>" +
-          "<td>Kr. " + book.price + ",-</td>" +
-        "</tr>");
+          "<td>" + book.edition  + "</td>" +
+          "<td>" + book.author + "</td>" +
+          "<td>" + book.isbn + "</td>" +
+          "</tr>");
     });
 
   });
