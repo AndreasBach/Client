dbedcbef299a4c21b14daa6dfb99600a25d71821
AndreasBach <andreasbach94@gmail.com>
2017-01-21 11:49:40 +0100

    Får userUpdate.js til at fungerer på alle parametre

--- js/userUpdate.js
+++ js/userUpdate.js
@@ -1,5 +1,5 @@
 
-//Opdater user
+//Opdatering af user
 
 $(document).ready(function () {
 
@@ -18,18 +18,44 @@ $(document).ready(function () {
 
             email: $("#inputEmail").val(),
 
-            mobilepay: +$("#inputMobilePay").prop("checked"),
 
-            cash: +$("#inputCash").prop("checked"),
 
-            transfer: +$ ("#inputTransfer").prop("checked")
+            var mobilepayIsChosen = 0;
+        if ($("input[name=mobilepay]:checked").val()) {
+            mobilepayIsChosen = 1;
 
+        }
+
+        var cashIsChosen = 0;
+        if ($("input[name=cash]:checked").val()) {
+            cashIsChosen = 1;
+        }
+
+        var transferIsChosen = 0;
+        if ($("input[name=transfer]:checked").val()) {
+            transferIsChosen = 1;
+
+        };
+
+        /*
+        JSON object
+         */
+        var user = {
+
+            username: $username,
+            password: $password,
+            phonenumber: $phonenumber,
+            address: $address,
+            email: $email,
+            mobilepay: mobilepayIsChosen,
+            cash: cashIsChosen,
+            transfer: transferIsChosen
         };
 
         // Update User
 
         SDK.User.update(user, function (err) {
-            if (err) throw JSON.stringify(err);
+            if (err) throw err;
 
             window.alert("Brugeren er nu opdateret");
 
